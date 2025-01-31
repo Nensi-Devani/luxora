bbe9ef6da2ae48fa46f1af73ba245ac98bb179ec
Nensi Devani <ndevani894@rku.ac.in>
2025-02-01 00:15:15 +0530

    Create categories table

--- database/migrations/2025_01_31_184115_create_categories_table.php
+++ database/migrations/2025_01_31_184115_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained();
             $table->string('name');
+            $table->string('image');
             $table->tinyInteger('status')->default(1)->comment('1=active,2=inactive');
             $table->timestamps();
         });
