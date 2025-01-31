7a6e38c23b1b7dc4335aea76f0c3c976d1eb3585
Nensi Devani <ndevani894@rku.ac.in>
2025-02-01 00:41:40 +0530

    Create products table

---
+++ database/migrations/2025_01_31_184700_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('metal_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('gemstone_id')->constrained();
+            $table->string('name');
+            $table->longText('description');
+            $table->enum('gender', ['men', 'women']);
+            $table->integer('discount')->nullable()->comment('Rs.');
+            $table->string('metal_weight')->nullable();
+            $table->string('metal_purity')->nullable();
+            $table->string('gemstone_weight')->nullable();
+            $table->string('gemstone_purity')->nullable();
+            $table->integer('no_of_gemstone')->nullable();
+            $table->boolean('express_delivery_available')->default(false)->comment('false=No, true=Yes');
+            $table->integer('express_delivery_charge')->nullable();
+            $table->string('warranty_period');
+            $table->string('images');
+            $table->string('certificate')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
