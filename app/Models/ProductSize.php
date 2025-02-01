f748f4ee49434b4610410e16fc8dc0f7c3c95707
Nensi Devani <ndevani894@rku.ac.in>
2025-02-01 23:53:46 +0530

    Create ProductSize model

--- app/Models/ProductSize.php
+++ app/Models/ProductSize.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductSize extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'size',
+        'stock',
+        'metal_weight',
+        'metal_purity',
+        'metal_price',
+        'gemstone_weight',
+        'gemstone_purity',
+        'gemstone_price',
+        'num_of_gemstone',
+        'making_charges',
+        'gst'
+    ];
 }
