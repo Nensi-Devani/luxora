80213188151c2b3ea5149f30f3a9398458366111
Nensi Devani <ndevani894@rku.ac.in>
2025-02-02 17:10:31 +0530

    Create enum for role

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('role')->default(0)->comment("0=User, 1=Admin");
+            $table->enum('role',['0','1'])->default(0)->comment("0=User, 1=Admin");
             $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
