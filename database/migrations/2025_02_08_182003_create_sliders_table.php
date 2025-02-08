9256b5e3ef7fa5fea3ac7a8df8efe87ceca2cc1f
Nensi Devani <ndevani894@rku.ac.in>
2025-02-08 23:55:18 +0530

    Create sliders table

---
+++ database/migrations/2025_02_08_182003_create_sliders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->string('image');
+            $table->string('redirect');
+            $table->enum('status',['0','1'])->default(1)->comment("1=Active,0=Inactive");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sliders');
+    }
+};
